4a3df1b88297410f879e35801afbf909cd91e6d3
kwabiwe <git@stackblitz.com>
2025-03-15 23:02:50 +0000

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'tf-gold': '#D4AF37',
+        'tf-black': '#1A1A1A',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Playfair Display', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
